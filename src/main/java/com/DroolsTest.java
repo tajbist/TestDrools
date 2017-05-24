d73ccd0734c0e5d9e50eca15589541fb4e578607
Taj Bist <tajbist2@gmail.com>
2017-05-24 16:46:31 -0500

    Adding new Drools Test Project

---
+++ src/main/java/com/DroolsTest.java
@@ -0,0 +1,50 @@
+package com;
+
+/**
+ * Created by Oops on 5/24/2017.
+ */
+import org.drools.compiler.compiler.DroolsParserException;
+import org.drools.compiler.compiler.PackageBuilder;
+import org.drools.core.RuleBase;
+import org.drools.core.RuleBaseFactory;
+import org.drools.core.WorkingMemory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class DroolsTest {
+
+    public static void main(String[] args) throws DroolsParserException,
+            IOException {
+        DroolsTest droolsTest = new DroolsTest();
+        droolsTest.executeDrools();
+    }
+
+    public void executeDrools() throws DroolsParserException, IOException {
+
+        PackageBuilder packageBuilder = new PackageBuilder();
+
+        String ruleFile = "/com/rule/Rules.drl";
+        InputStream resourceAsStream = getClass().getResourceAsStream(ruleFile);
+
+        Reader reader = new InputStreamReader(resourceAsStream);
+        packageBuilder.addPackageFromDrl(reader);
+        org.drools.core.rule.Package rulesPackage = packageBuilder.getPackage();
+        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+        ruleBase.addPackage(rulesPackage);
+
+        WorkingMemory workingMemory = ruleBase.newStatefulSession();
+
+        Product product = new Product();
+        product.setType("diamond");
+
+        workingMemory.insert(product);
+        workingMemory.fireAllRules();
+
+        System.out.println("The discount for the product " + product.getType()
+                + " is " + product.getDiscount());
+    }
+
+}
